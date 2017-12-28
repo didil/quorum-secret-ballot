8440ef687c6a9609420978ea806aeed1e2285e5a
didil <haritahadil@yahoo.fr>
2017-12-28 14:59:01 +0100

    fix + truffle config

---
+++ quorum-secret-ballot/migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+var SecretBallot = artifacts.require("./SecretBallot.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(SecretBallot,
+    "0xca843569e3427144cead5e4d5999a3d0ccf92b8e",   // Company A address
+    "0x0fbdc686b912d7722dc86510934589e0aaf3b55a",   // Company B address
+    "0x9186eb3d20cbd1f5f992a950d808c4495153abd5",   // Company C address
+    {
+      privateFor: [
+        "BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo=", // Regulator Node
+        "QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc=", // Company A Node
+        "1iTZde/ndBHvzhcl7V68x44Vx7pl8nwx9LqnM/AfJUg=", // Company B Node
+        "oNspPPgszVUFw0qmGFfWwh1uxVUXgvBxleXORHj07g8=", // Company C Node
+      ]
+    });
+};
---
+++ quorum-secret-ballot/truffle.js
@@ -0,0 +1,32 @@
+module.exports = {
+  networks: {
+    development: { // deploying from regulator
+      host: "127.0.0.1",
+      port: 22000, // was 8545
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000
+    },
+    a:  {
+      host: "127.0.0.1",
+      port: 22001,
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000
+    },
+    b:  {
+      host: "127.0.0.1",
+      port: 22002,
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000
+    },
+    c:  {
+      host: "127.0.0.1",
+      port: 22003,
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000
+    }
+  }
+};
